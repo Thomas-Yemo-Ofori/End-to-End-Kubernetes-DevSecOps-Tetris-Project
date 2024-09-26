93d621394c1863474a970d47dac0fc79416ae413
Nii-Yemo-Ofori <160476489+Thomas-Yemo-Ofori@users.noreply.github.com>
2024-09-26 15:13:05 +0000

    Update backend.tf

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "my-ews-baket1"
-    region         = "us-east-1"
-    key            = "End-to-End-Kubernetes-DevSecOps-Tetris-Project/EKS-TF/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
+    bucket         = "tetris-s3-bucket"
+    region         = "us-west-2"
+    key            = "End-to-End-Kubernetes-DevSecOps-Tetris-Project/Jenkins-Server-TF/.terraform/terraform.tfstate"
+    dynamodb_table = "Lock-files"
     encrypt        = true
   }
   required_version = ">=0.13.0"
